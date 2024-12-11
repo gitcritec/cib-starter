03f53cf8b1246321c30bd5536bb311731f94e747
Leo F <l3o.f3rr3ira@gmail.com>
2024-12-11 17:39:41 +0000

    fix filter

--- src/app/components/layouts/header.tsx
+++ src/app/components/layouts/header.tsx
@@ -8,8 +8,8 @@ import LanguageSelector from "../elements/languageSelector";
 
 
 const Header = ({ logo, menus, allLangsActives, lang_id }: HeaderProps) => {
-    const headerMenu = fetchMenuChildren(menus,'2');
-  
+    const headerMenu = fetchMenuChildren(menus,2);
+
     return (
         <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container flex h-16 items-center justify-between">
